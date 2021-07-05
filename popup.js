1dd14142d096259e4d25b2743b3878a7037f9338
MMMKassem <m.m.m.kassem@sms.ed.ac.uk>
2021-07-05 11:58:12 +0100

    Add Top Alexa Sites

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 var total = 0;
+var sendTofetch = [];
 
 function set(id, start, end, noacc) {
   var length = Math.round(end - start);
@@ -11,6 +12,46 @@ function set(id, start, end, noacc) {
     'background-position-x:' + (x >= 300 ? 299 : x) + 'px;';
 }
 
+function collapsible_handle(){
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
+
+function fetchpages(arra){
+  // document.addEventListener('DOMContentLoaded', function() {
+    // var buttonelement = document.getElementById('alexab');
+    var buttonelement = document.getElementById('a01');
+    buttonelement.addEventListener('click', function() {
+      for (i = 0; i < 5; i++){
+        // alert("Hello!");
+        var getou = arra[i];
+        fetch(getou)
+        .then(response => response.text())
+
+        // .then(data => document.getElementById("demo3").innerHTML = data);
+        // var x = document.createElement("div");
+        // if(document.readyState === 'complete'){
+          // startCollect2();
+          // alert("Hello!2");
+          // }
+        }
+        setTimeout(getTiming_forAlexa, 2000);
+      }, false);
+    // }, false);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var checkboxelement = document.getElementById('chk1');
   checkboxelement.addEventListener('change', function() {
@@ -20,7 +61,60 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById("demo").innerHTML = "";
     }
   }, false);
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+
+  var xmlhttp = new XMLHttpRequest();
+  var url = "http://34.89.29.21/top500.json";
+
+
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myArr = JSON.parse(this.responseText);
+      var i;
+      var out = "";
+      for(i = 1; i < 6; i++) {
+        var x = Math.floor((Math.random() * 10) + 1);
+        sendTofetch.push("https://www."+ myArr[x]);
+        out += '>> https://www.'+ myArr[x] + '<br>';
+      }
+      document.getElementById("id01").innerHTML = out;
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+  fetchpages(sendTofetch);
 }, false);
+
+function getTiming_forAlexa(){
+  var resources = performance.getEntriesByType("resource");
+  var outt = "";
+  var k = 0;
+  while(k < sendTofetch.length){
+    for(var i=0; i < resources.length; i++){
+      if(resources[i].initiatorType == "fetch"){
+        if(sendTofetch[k]+"/" == resources[i].name){
+          outt += Math.round(resources[i].duration) + ' ms' + '<br>';
+          k++;
+        }
+      }
+    }
+  }
+  document.getElementById("id02").innerHTML = outt;
+}
 
 function showCustomer() {
   str = document.getElementById("tabUrl").innerHTML;
